3178da62f9fdc1c798ffe0e3a494deffb2de98d3
Anton Bandurka <aabandurka@yandex.ru>
2020-10-20 23:19:35 +0300

    added taxes service

---
+++ DisributedLockViaZooKeeper.Taxes.WepApi/Controllers/TaxesController.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DisributedLockViaZooKeeper.Taxes.WepApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxesController : ControllerBase
+    {
+        private readonly TaxRepository _repository;
+
+        public TaxesController(TaxRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{index}")]
+        public IActionResult GetTax([Required]string index)
+        {
+            return Ok(_repository.GetTax(index));
+        }
+        
+        [HttpGet("{index}/hits")]
+        public IActionResult GetHits([Required]string index)
+        {
+            return Ok(_repository.GetHits(index));
+        }
+
+        [HttpDelete("{index}")]
+        public IActionResult Delete([Required]string index)
+        {
+            _repository.Clear(index);
+            return Ok();
+        }
+    }
+}
---
+++ DisributedLockViaZooKeeper.Taxes.WepApi/TaxRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace DisributedLockViaZooKeeper.Taxes.WepApi
+{
+    public class TaxRepository
+    {
+        private readonly ConcurrentDictionary<string, int> _taxes = new ConcurrentDictionary<string, int>();
+        private readonly Random Rnd = new Random();
+        
+        public string GetTax(string index)
+        {
+            _taxes[index] = _taxes.ContainsKey(index) 
+                ? _taxes[index] + 1
+                : 0;
+
+            return (Rnd.NextDouble() * 100).ToString();
+        }
+
+        public void Clear(string index) => 
+            _taxes.TryRemove(index, out _);
+
+        public int GetHits(string index) =>
+            _taxes.TryGetValue(index, out var result) 
+                ? result 
+                : 0;
+    }
+}
